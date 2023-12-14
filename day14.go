c542b26886d7c8092a2bec8f9749389c9ee7e621
Roger <roger.lampe@gmail.com>
2023-12-14 13:20:05 -0500

    too slow

--- day14.go
+++ day14.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"sort"
+	"time"
 )
 
 const ROCK = 'O'
@@ -11,11 +13,12 @@ const EMPT = '.'
 
 type platform []string
 
-func day14(scanner *bufio.Scanner, isPart2 bool) string {
-	if isPart2 {
-		log("it sure is part 2")
+func (p platform) Len() int {
+	return len(p)
+}
 
-	}
+func day14(scanner *bufio.Scanner, isPart2 bool) string {
+
 	grandTotal := 0
 	//day 14 is one grid
 	platform := platform{}
@@ -28,7 +31,27 @@ func day14(scanner *bufio.Scanner, isPart2 bool) string {
 		check(err)
 	}
 
-	platform.tilt(UP)
+	if isPart2 {
+		start := time.Now()
+		log("it sure is part 2")
+		//start cyclin
+		for i := 0; i < 1000000000; i++ {
+			platform.tilt(UP)
+			platform.tilt(LEFT)
+			platform.tilt(DOWN)
+			platform.tilt(RIGHT)
+			if i%10000 == 0 {
+				fmt.Print("k")
+			}
+			if i%1000000 == 0 {
+				stop := time.Since(start)
+				log("one Million", stop)
+			}
+
+		}
+	} else {
+		platform.tilt(UP)
+	}
 
 	grandTotal += platform.weigh(UP)
 
@@ -52,11 +75,49 @@ func (p platform) weigh(direction dir) (totalWeight int) {
 	return totalWeight
 }
 
-func (p *platform) tilt(direction dir) {
+func (p platform) tilt(direction dir) {
 
-	//Hmm, we need to range differently for other direcitons. here is up
-	for rowI, line := range *p {
-		for colI := range line {
+	// if direction == DOWN {
+	// 	//we should go bottom up, reverse p
+	// 	for i, j := 0, len(p)-1; i < j; i, j = i+1, j-1 {
+	// 		p[i], p[j] = p[j], p[i]
+	// 	}
+	// }
+
+	// if direction == RIGHT {
+	// 	//we should go right to left, reverse each line
+	// 	//we should go bottom up, reverse line. Strings are immutable, so
+	// 	for l, line := range p {
+	// 		revLine := []byte(line)
+	// 		for i, j := 0, len(line)-1; i < j; i, j = i+1, j-1 {
+	// 			revLine[i], revLine[j] = line[j], line[i]
+	// 		}
+	// 		p[l] = string(revLine)
+	// 	}
+	// }
+
+	//make a list of indexes we can reverse.
+	rowOrder := []int{}
+	for index := range p {
+		rowOrder = append(rowOrder, index)
+	}
+	if direction == DOWN {
+		sort.Sort(sort.Reverse(sort.IntSlice(rowOrder)))
+	}
+
+	for _, rowI := range rowOrder {
+		line := p[rowI]
+		//make a list of indexes for line we can reverse
+		lineOrder := []int{}
+		for index := range line {
+			lineOrder = append(lineOrder, index)
+		}
+		if direction == RIGHT {
+			sort.Sort(sort.Reverse(sort.IntSlice(lineOrder)))
+		}
+		for _, colI := range lineOrder {
+			//if we're going right, we need to reverse line
+
 			if line[colI] == ROCK {
 				rockRow := rowI
 				rockCol := colI
@@ -69,6 +130,7 @@ func (p *platform) tilt(direction dir) {
 			}
 		}
 	}
+
 }
 
 func (p platform) canMove(rowI, colI int, UP dir) bool {
